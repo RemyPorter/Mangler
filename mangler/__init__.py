377d2d01703952895400743e051dc610bf2e3394
Remy Porter <remyporter@jetpackshark.com>
2015-12-13 16:53:27 -0500

    Made hits-per-second configurable through the command line.

--- mangler/__init__.py
+++ mangler/__init__.py
@@ -1,10 +1,9 @@
 from . import load
 from . import analyzer
 
-def process(filename, outputfile, limit=None):
+def process(filename, outputfile, hits_per_second=3, limit=None):
     channels,frame_rate = load.read(filename, limit)
     target = frame_rate
-    hits = 4 * len(channels[0])/target
+    hits = hits_per_second * len(channels[0])/target
     data = analyzer.mangle(channels, num_hits=hits, block_size=target)
-    print "Writing..."
     load.write(outputfile, channels, frame_rate)
