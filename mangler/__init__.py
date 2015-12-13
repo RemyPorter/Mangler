6a61dcec9e6e3587082d75a051f713c345edb8be
Remy Porter <remyporter@jetpackshark.com>
2015-12-13 16:44:17 -0500

    Modified to accept some basic CLI inputs, like filenames, instead of hard coding.

--- mangler/__init__.py
+++ mangler/__init__.py
@@ -1,10 +1,10 @@
 from . import load
 from . import analyzer
 
-def process(filename, limit=None):
+def process(filename, outputfile, limit=None):
     channels,frame_rate = load.read(filename, limit)
     target = frame_rate
     hits = 4 * len(channels[0])/target
     data = analyzer.mangle(channels, num_hits=hits, block_size=target)
     print "Writing..."
-    load.write("output.wav", channels, frame_rate)
+    load.write(outputfile, channels, frame_rate)
--- mangler/__main__.py
+++ mangler/__main__.py
@@ -1,4 +1,13 @@
 import sys
+import argparse
 from . import process
-fname = sys.argv[1]
-process(fname)
+
+parser = argparse.ArgumentParser(description="Mangle an audio file, using cut-up techniques based on dadaist and surrealist art.")
+parser.add_argument("input", metavar="input", type=str, help="the input file")
+parser.add_argument("output", metavar="output", nargs="?", help="the output file destination, defaults to output.wav")
+args =parser.parse_args()
+fname = args.input
+oname = args.output
+if oname is None:
+    oname = "output.wav"
+process(fname, oname)
