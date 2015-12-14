3067451b36f3255c07dfe8cc77a73a57e2787bef
Remy Porter <remyporter@jetpackshark.com>
2015-12-14 17:57:21 -0500

    Added some hooks to permit operation weights to get passed in. This is going to take some refactoring.

--- mangler/__init__.py
+++ mangler/__init__.py
@@ -1,9 +1,9 @@
 from . import load
 from . import analyzer
 
-def process(filename, outputfile, hits_per_minute=180, limit=None):
+def process(filename, outputfile, hits_per_minute=180, limit=None, ops=None):
     channels,frame_rate = load.read(filename, limit)
     target = frame_rate
     hits = hits_per_minute * len(channels[0])/(target*60)
-    data = analyzer.mangle(channels, num_hits=hits, block_size=target)
+    data = analyzer.mangle(channels, num_hits=hits, block_size=target, ops=ops)
     load.write(outputfile, channels, frame_rate)
