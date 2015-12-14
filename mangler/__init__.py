3519cfc578ea38b40d9ee86ac0b066710c664c2e
Remy Porter <remyporter@jetpackshark.com>
2015-12-14 16:01:43 -0500

    Changed to use 'hits per minute' instead of hits per second

--- mangler/__init__.py
+++ mangler/__init__.py
@@ -1,9 +1,9 @@
 from . import load
 from . import analyzer
 
-def process(filename, outputfile, hits_per_second=3, limit=None):
+def process(filename, outputfile, hits_per_minute=180, limit=None):
     channels,frame_rate = load.read(filename, limit)
     target = frame_rate
-    hits = hits_per_second * len(channels[0])/target
+    hits = hits_per_minute * len(channels[0])/(target*60)
     data = analyzer.mangle(channels, num_hits=hits, block_size=target)
     load.write(outputfile, channels, frame_rate)
--- mangler/__main__.py
+++ mangler/__main__.py
@@ -5,13 +5,13 @@ from . import process
 parser = argparse.ArgumentParser(description="Mangle an audio file, using cut-up techniques based on dadaist and surrealist art.")
 parser.add_argument("input", metavar="input", type=str, help="the input file")
 parser.add_argument("output", metavar="output", nargs="?", help="the output file destination, defaults to output.wav")
-parser.add_argument("--hps", metavar="hps", type=int, nargs="?", help="the number of 'cuts' or 'hits' to make per second of audio, on average. Default is 3.")
+parser.add_argument("--hpm", metavar="hpm", type=int, nargs="?", help="the number of 'cuts' or 'hits' to make per minute of audio, on average. Default is 180, or 3 hits per second.")
 args =parser.parse_args()
 fname = args.input
 oname = args.output
-hps = args.hps
-if hps is None:
-    hps = 3
+hpm = args.hpm
+if hpm is None:
+    hpm = 180
 if oname is None:
     oname = "output.wav"
-process(fname, oname, hps)
+process(fname, oname, hpm)
