15d5a9547ebc868a2daa0452426aa15e42ca5ef7
Remy Porter <remyporter@jetpackshark.com>
2015-12-22 17:37:56 -0500

    Hey, a basic working version is now complete. What do you know!

--- mangler/__main__.py
+++ mangler/__main__.py
@@ -1,17 +1,30 @@
 import sys
 import argparse
-from . import process
+from . import load
+from .randomizers import *
+from . import operations as o
 
 parser = argparse.ArgumentParser(description="Mangle an audio file, using cut-up techniques based on dadaist and surrealist art.")
-parser.add_argument("input", metavar="input", type=str, help="the input file")
-parser.add_argument("output", metavar="output", nargs="?", help="the output file destination, defaults to output.wav")
-parser.add_argument("--hpm", metavar="hpm", type=int, nargs="?", help="the number of 'cuts' or 'hits' to make per minute of audio, on average. Default is 180, or 3 hits per second.")
+parser.add_argument("input", metavar="input", type=str,
+    help="the input file")
+parser.add_argument("output", metavar="output", nargs="?",
+    help="the output file destination, defaults to output.wav",
+    default="output.wav")
+parser.add_argument("--hpm", metavar="hpm", type=int, nargs="?",
+    help="how many hits/cuts to make per minute.", default=180)
 args =parser.parse_args()
-fname = args.input
-oname = args.output
+
+inputfile = args.input
+outputfile = args.output
 hpm = args.hpm
-if hpm is None:
-    hpm = 180
-if oname is None:
-    oname = "output.wav"
-process(fname, oname, hpm)
+
+stream, rate = load.read(inputfile)
+size = len(stream[0])
+
+hits = hpm * size / (rate*60)
+p = Population(o.__all__)
+for i in range(hits):
+    op = p.get().generate(rate, size)
+    op(stream)
+
+load.write(outputfile, stream, rate)
