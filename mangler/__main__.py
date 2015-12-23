a896363799204ea92a16d829cc9fe04ab56a5722
Remy Porter <remyporter@jetpackshark.com>
2015-12-23 15:48:02 -0500

    Refactored the main method to be slightly cleaner.

--- mangler/__main__.py
+++ mangler/__main__.py
@@ -4,27 +4,34 @@ from . import load
 from .randomizers import *
 from . import operations as o
 
-parser = argparse.ArgumentParser(description="Mangle an audio file, using cut-up techniques based on dadaist and surrealist art.")
-parser.add_argument("input", metavar="input", type=str,
-    help="the input file")
-parser.add_argument("output", metavar="output", nargs="?",
-    help="the output file destination, defaults to output.wav",
-    default="output.wav")
-parser.add_argument("--hpm", metavar="hpm", type=int, nargs="?",
-    help="how many hits/cuts to make per minute.", default=180)
-args =parser.parse_args()
+def build_arguments():
+    parser = argparse.ArgumentParser(description="Mangle an audio file, using cut-up techniques based on dadaist and surrealist art.")
+    parser.add_argument("input", metavar="input", type=str,
+        help="the input file")
+    parser.add_argument("output", metavar="output", nargs="?",
+        help="the output file destination, defaults to output.wav",
+        default="output.wav")
+    parser.add_argument("--hpm", metavar="hpm", type=int, nargs="?",
+        help="how many hits/cuts to make per minute.", default=180)
+    return parser
 
-inputfile = args.input
-outputfile = args.output
-hpm = args.hpm
+def main():
+    args = build_arguments().parse_args()
 
-stream, rate = load.read(inputfile)
-size = len(stream[0])
+    inputfile = args.input
+    outputfile = args.output
+    hpm = args.hpm
 
-hits = hpm * size / (rate*60)
-p = Population(o.__all__)
-for i in range(hits):
-    op = p.get().generate(rate, size)
-    op(stream)
+    stream, rate = load.read(inputfile)
+    size = len(stream[0])
 
-load.write(outputfile, stream, rate)
+    hits = hpm * size / (rate*60)
+    p = Population(o.__all__)
+    for i in range(hits):
+        op = p.get().generate(rate, size)
+        op(stream)
+
+    load.write(outputfile, stream, rate)
+
+if __name__ == "__main__":
+    main()
